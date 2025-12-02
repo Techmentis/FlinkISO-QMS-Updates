0ea0944bbfde0410b48d83fcaba802940b17d9e2
FlinkISO <Techmentis@users.noreply.github.com>
2025-12-03 00:35:17 +0530

    Update SQL fix
    Update SQL fix

--- app/webroot/updates/updates.sql
+++ app/webroot/updates/updates.sql
@@ -1,27 +1,19 @@
-ALTER TABLE `document_downloads` CHANGE `reccord_id` `record_id` VARCHAR(36)  NULL DEFAULT NULL;
-ALTER TABLE `document_downloads` ADD `name` VARCHAR(255) NULL AFTER `sr_no`;
-ALTER TABLE `companies` ADD `change_management_table` VARCHAR(36) NULL AFTER `version`, ADD `change_management_table_fields` TEXT NULL AFTER `change_management_table`;
-
-
-
-ALTER TABLE `graph_panels` ADD `data_type` INT(1) NULL DEFAULT '0' COMMENT '0=count,1=sum,2=avg' AFTER `graph_type`;
-ALTER TABLE `graph_panels` ADD `value_field` VARCHAR(255) NULL AFTER `data_type`;
-ALTER TABLE `graph_panels` ADD `position` INT NULL DEFAULT'0' AFTER `color`, ADD `size` INT NULL DEFAULT '3' AFTER `position`;
-ALTER TABLE `graph_panels` ADD `title` VARCHAR(255) NULL DEFAULT NULL AFTER `sr_no`;
-
-
-ALTER TABLE `qc_documents` ADD `name` VARCHAR(255)  NOT NULL AFTER `sr_no`;
-ALTER TABLE `processes` CHANGE `process_objecttive_and_metrics` `process_objective_and_metrics` TEXT NULL DEFAULT NULL;
-
-ALTER TABLE `users` ADD `pwd_last_modified` DATETIME NULL AFTER `email_token_expires`;
-
-ALTER TABLE `qc_documents` ADD `additional_clauses` VARCHAR(255) NULL AFTER `standard_id`;
-
-ALTER TABLE `qc_documents` ADD `data_update_type` INT(1) NULL DEFAULT '0' AFTER `schedule_id`;
-ALTER TABLE `users` ADD `is_creator` TINYINT(1) NULL DEFAULT '1' AFTER `is_approver`, ADD `is_publisher` TINYINT(1) NULL DEFAULT '1' AFTER `is_creator`;
-ALTER TABLE `custom_tables` ADD `creators` TEXT NULL AFTER `users`, ADD `editors` TEXT NULL AFTER `creators`, ADD `viewers` TEXT NULL AFTER `editors`, ADD `approvers` TEXT NULL AFTER `viewers`;
-ALTER TABLE `files` ADD `versions` TEXT NULL AFTER `version_keys`;
-ALTER TABLE `qc_documents` ADD `allow_download` TINYINT(1) NULL AFTER `editors`, ADD `allow_print` TINYINT(1) NULL AFTER `allow_download`;
-ALTER TABLE `custom_triggers` ADD `notify_departments` TINYINT(1) NULL AFTER `notify_hods`, ADD `notify_branches` TINYINT(1) NULL AFTER `notify_departments`, ADD `notify_designations` TINYINT(1) NULL AFTER `notify_branches`;
-ALTER TABLE `users` CHANGE `password` `password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
-ALTER TABLE `qc_documents` ADD `and_or_condition` TINYINT(1) NULL DEFAULT '0' COMMENT '0=And;1=OR' AFTER `designations`;
+ALTER TABLE document_downloads CHANGE reccord_id record_id VARCHAR(36)  NULL DEFAULT NULL;
+ALTER TABLE document_downloads ADD name VARCHAR(255) NULL AFTER sr_no;
+ALTER TABLE companies ADD change_management_table VARCHAR(36) NULL AFTER version, ADD change_management_table_fields TEXT NULL AFTER change_management_table;
+ALTER TABLE graph_panels ADD data_type INT(1) NULL DEFAULT 0 AFTER graph_type;
+ALTER TABLE graph_panels ADD value_field VARCHAR(255) NULL AFTER data_type;
+ALTER TABLE graph_panels ADD position INT NULL DEFAULT 0 AFTER color, ADD size INT NULL DEFAULT 3 AFTER position;
+ALTER TABLE graph_panels ADD title VARCHAR(255) NULL DEFAULT NULL AFTER sr_no;
+ALTER TABLE qc_documents ADD name VARCHAR(255)  NOT NULL AFTER sr_no;
+ALTER TABLE processes CHANGE process_objecttive_and_metrics process_objective_and_metrics TEXT NULL DEFAULT NULL;
+ALTER TABLE users ADD pwd_last_modified DATETIME NULL AFTER email_token_expires;
+ALTER TABLE qc_documents ADD additional_clauses VARCHAR(255) NULL AFTER standard_id;
+ALTER TABLE qc_documents ADD data_update_type INT(1) NULL DEFAULT 0 AFTER schedule_id;
+ALTER TABLE users ADD is_creator TINYINT(1) NULL DEFAULT 1 AFTER is_approver, ADD is_publisher TINYINT(1) NULL DEFAULT 1 AFTER is_creator;
+ALTER TABLE custom_tables ADD creators TEXT NULL AFTER users, ADD editors TEXT NULL AFTER creators, ADD viewers TEXT NULL AFTER editors, ADD approvers TEXT NULL AFTER viewers;
+ALTER TABLE files ADD versions TEXT NULL AFTER version_keys;
+ALTER TABLE qc_documents ADD allow_download TINYINT(1) NULL AFTER editors, ADD allow_print TINYINT(1) NULL AFTER allow_download;
+ALTER TABLE custom_triggers ADD notify_departments TINYINT(1) NULL AFTER notify_hods, ADD notify_branches TINYINT(1) NULL AFTER notify_departments, ADD notify_designations TINYINT(1) NULL AFTER notify_branches;
+ALTER TABLE users CHANGE password password VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL;
+ALTER TABLE qc_documents ADD and_or_condition TINYINT(1) NULL DEFAULT 0 AFTER designations;
